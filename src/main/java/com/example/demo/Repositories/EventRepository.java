1da5fdde3b5344bc785db265e1fd28eaad0844f5
Sreenadh27 <160207175+Sreenadh27@users.noreply.github.com>
2024-12-18 20:28:53 +0530

    Initial

---
+++ src/main/java/com/example/demo/Repositories/EventRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Model.Event;
+
+import java.util.List;
+
+
+public interface EventRepository extends JpaRepository<Event,String>{
+	
+	
+	public Event findByEventName(String eventName);
+	
+}
